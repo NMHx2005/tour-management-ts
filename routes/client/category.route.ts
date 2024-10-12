db716bb432fc494dc0fcd4fd827439717f9faa57
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2024-10-12 23:11:32 +0700

    Làm trang danh mục tour (bên client)

---
+++ routes/client/category.route.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+import * as controller from "../../controllers/client/category.controller";
+
+router.get("/", controller.index);
+
+
+export const categoryRoute = router;
--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import { tourRoute } from "./tour.route";
+import { categoryRoute } from "./category.route";
 
 export const routesClient = (app: Express) => {
     app.use("/tours", tourRoute);
+    app.use("/categories", categoryRoute);
 }
