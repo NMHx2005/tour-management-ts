aca0af46c3bf39ee3a5dbcce62629a0f664eb134
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2024-10-13 21:44:49 +0700

    Làm giao diện trang giỏ hàng

---
+++ routes/client/cart.route.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+import * as controller from "../../controllers/client/cart.controller";
+
+router.get("/", controller.index);
+
+
+export const cartRoute = router;
